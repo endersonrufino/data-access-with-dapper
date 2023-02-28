1f08cbba0c7d934d6421781d4568b6c237f88667
Enderson Rufino Amorim <endersonrufino@gmail.com>
2023-02-28 18:54:14 -0300

    separação de métodos

--- Animes/Models/Anime.cs
+++ Animes/Models/Anime.cs
@@ -1,7 +1,7 @@
 namespace Animes.Models
 {
     public class Anime
-    {
+    {        
         public int Id { get; set; }
         public string AnimeName { get; set; }
     }
--- Animes/Program.cs
+++ Animes/Program.cs
@@ -10,8 +10,38 @@ class Program
     {
         const string connectionString = "Data Source=DESKTOP-FVOS0IE\\SQLEXPRESS;Initial Catalog=qwerty;Integrated Security=True;TrustServerCertificate=True";
 
-        using(var connection = new SqlConnection(connectionString)){
-            var animes = connection.Query<Anime>("SELECT [Id], [AnimeName] FROM Anime").ToList();
+        using (var connection = new SqlConnection(connectionString))
+        {
+            //CreateAnime(connection);
+            UpdateAnime(connection);
+            GetAnimes(connection);
         }
+    }
+
+    static void GetAnimes(SqlConnection connection)
+    {
+        var animes = connection.Query<Anime>("SELECT [Id], [AnimeName] FROM Anime").ToList();
+
+        foreach (var item in animes)
+        {
+            Console.WriteLine($"{item.Id} - {item.AnimeName}");
+        }
+    }
+
+    static void CreateAnime(SqlConnection connection)
+    {
+        var anime = new Anime();
+        anime.AnimeName = "Naruto";
+
+        var query = "INSERT INTO [Anime] VALUES (@AnimeName)";
+
+        connection.Execute(query, new { anime.AnimeName });
+    }
+
+    static void UpdateAnime(SqlConnection connection)
+    {
+        var query = "UPDATE [Anime] SET [AnimeName]=@AnimeName WHERE [Id]=@id";
+
+        connection.Execute(query, new { id = 9,  AnimeName = "Boruto"});
     }
 }
